dbb3d1e7ae7c31792a00accc1938f237a464826c
s-weia <s-weia@54S50037293.it.bsd405.org>
2017-11-11 17:09:00 -0800

    UI

---
+++ src/main/Board.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel{
+	@Override
+	public void paintComponent(Graphics g) {
+		
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class Main {
 	public static void main(String args[]) {
+		JFrame frame = new JFrame("Derp");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		Board b = new Board();
+		b.setPreferredSize(new Dimension(500,500));
+		
+		frame.add(b);
+		frame.pack();
+		frame.setVisible(true);		
 	}
 }
